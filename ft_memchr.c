8cd4a8c10625a1f31b9b534f7f8caaa1ae197ef5
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-10 17:50:21 +0100

    ft_memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:00 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/10 17:31:03 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/10 17:37:44 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	ch;
 	unsigned char	*searchstring;
 
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,30 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:03 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/09 12:28:05 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/10 17:49:26 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+	i = 0;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	while (*(str1 + i) == *(str2 + i))
+	{
+		if (i == n)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (*(str1 + i) - *(str2 + i));
+}
