ec608ed66310e28545d3499aba653a335cb69ee9
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-10 20:10:49 +0100

    ft_subrstr

--- ft_substr.c
+++ ft_substr.c
@@ -6,8 +6,34 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:29:08 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/09 12:29:10 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/10 20:08:34 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substring;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+	{
+		return (0);
+	}
+	substring = malloc(len);
+	while (*(s + start + i) != '\0')
+	{
+		if (i == len)
+		{
+			break ;
+		}
+		*(substring + i) = *(s + start + i);
+		i++;
+	}
+	*(substring + i) = '\0';
+	return (substring);
+}
