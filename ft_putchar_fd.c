2ed7debd19cf1ac7f32ba10f51161fd6dabfa518
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-11 17:43:03 +0100

    finished mandatory part

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,8 +6,15 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:15 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/09 12:28:16 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/11 17:38:45 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putchar_fd(char c, int fd);
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,8 +6,15 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:21 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/09 12:28:23 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/11 17:39:01 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd);
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	ft_putstr_fd(ft_itoa(n), fd);
+}
