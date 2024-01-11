403ed93053f2353015c8b8c9e6021dd64f1cb985
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-11 14:24:16 +0100

    finished ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,12 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:27:11 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/10 23:10:35 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/11 14:23:18 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 char	*ft_itoa(int n);
 int		intlen(int n);
 char	*string_alloc(int n, int i);
@@ -21,52 +21,46 @@ char	*ft_itoa(int n)
 	char	*str;
 	int		i;
 
-	if (!n)
+	if (n == 0)
+	{
+		return ("0");
+	}
+	else if (!n)
 	{
 		return (NULL);
 	}
-	if (n == 0)
+	else if (n == -2147483648)
 	{
-		printf("-2147483648");
 		return ("-2147483648");
-	}
-	if (n == 0)
-	{
-		printf("1");
-		return ("0");
 	}
 	i = intlen(n);
 	str = string_alloc(n, i);
 	return (str);
 }
 
-
-
 int	intlen(int n)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
 	while (n != 0)
 	{
 		n = n / 10;
 		i++;
 	}
-	printf("Länge: %i\n", i);
 	return (i);
 }
 
 char	*string_alloc(int n, int i)
 {
 	char	*nbr_str;
-printf("Länge später: %i\n", i);
+
 	if (n < 0)
 	{
 		i++;
 		n = n * -1;
 		nbr_str = malloc(i);
 		*nbr_str = '-';
-		printf("HALLO!; %i\n", n);
 	}
 	else
 	{
@@ -78,7 +72,6 @@ printf("Länge später: %i\n", i);
 	{
 		*(nbr_str + i) = n % 10 + 48;
 		n = n / 10;
-		printf("3, %s, %i\n", nbr_str + i, n);
 		i--;
 	}
 	return (nbr_str);
