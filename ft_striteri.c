ef7229605d7fb5a1d60c5c522def0a5e823ddfbb
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-11 17:27:37 +0100

    ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,8 +6,25 @@
 /*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:37 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/09 12:28:39 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/11 17:27:16 by dflugel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (s == NULL || f == NULL)
+	{
+		return ;
+	}
+	while (*(s + i) != '\0')
+	{
+		f(i, (s + i));
+		i++;
+	}
+}
