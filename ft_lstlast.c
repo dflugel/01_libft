39d13952b731466358cde79c9a996c09339dc48e
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-17 21:52:35 +0100

    fixed lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: madwingg <madwingg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:27:46 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/11 18:17:17 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/17 21:52:18 by madwingg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	*current;
 
 	current = lst;
-	while (current->next != NULL)
+	while (current != NULL)
 	{
+		if (current->next == NULL)
+		{
+			return (current);
+		}
 		current = current->next;
 	}
-	return (current);
+	return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: madwingg <madwingg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:55 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/10 18:47:41 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/17 21:50:26 by madwingg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,20 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (*(s1 + i) == *(s2 + i) && n > 0)
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (*(s1 + i) == *(s2 + i) && i < n)
 	{
 		if (*(s1 + i) == '\0' || *(s2 + i) == '\0')
 		{
 			break ;
 		}
 		i++;
-		n--;
 	}
 	return (*(s1 + i) - *(s2 + i));
 }
