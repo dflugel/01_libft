cf561ff48e590cb235bd152bd3553280dd65f259
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-18 13:33:44 +0100

    fixed atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: madwingg <madwingg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:26:43 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/18 13:11:58 by madwingg         ###   ########.fr       */
+/*   Updated: 2024/01/18 13:32:21 by madwingg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ int	ft_atoi(const char *nptr)
 
 	nbr = 0;
 	nbr_sign = 1;
+	while (*nptr == 32 || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
 	if (*nptr == '-')
 	{
 		nbr_sign = -1;
 		nptr++;
 	}
+	else if (*nptr == '+')
+		nptr++;
 	while (*nptr != '\0')
 	{
 		if (*nptr < 48 || *nptr > 57)
-		{
-			return (0);
-		}
+			break ;
 		else if ((nbr * nbr_sign) == -214748364 && *nptr == '8')
-		{
 			return (-2147483648);
-		}
 		nbr = nbr * 10 + (*nptr - 48);
 		nptr++;
 	}
