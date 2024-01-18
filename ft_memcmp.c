8edacf1f42da82104a5b3f0b50b482756d025b91
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-18 12:57:09 +0100

    fixed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dflugel <dflugel@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: madwingg <madwingg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:03 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/10 17:51:14 by dflugel          ###   ########.fr       */
+/*   Updated: 2024/01/18 12:55:00 by madwingg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
+	size_t		i;
+	const char	*str1;
+	const char	*str2;
 
 	i = 0;
-	str1 = (unsigned char *) s1;
-	str2 = (unsigned char *) s2;
-	while (*(str1 + i) == *(str2 + i))
+	str1 = s1;
+	str2 = s2;
+	while (i < n)
 	{
-		if (i == n)
+		if (*(str1 + i) != *(str2 + i))
 		{
-			return (0);
+			return ((unsigned char)*(str1 + i) - (unsigned char)*(str2 + i));
 		}
 		i++;
 	}
-	return (*(str1 + i) - *(str2 + i));
+	return (0);
 }
