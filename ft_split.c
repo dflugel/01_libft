1b14283bc246d1e8cc3a819960a764f014fea286
Daniel Flügel <dflugel@student.42heilbronn.de>
2024-01-22 02:34:21 +0100

    fixed split

--- ft_split.c
+++ ft_split.c
@@ -6,83 +6,101 @@
 /*   By: madwingg <madwingg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:28:28 by dflugel           #+#    #+#             */
-/*   Updated: 2024/01/19 17:47:20 by madwingg         ###   ########.fr       */
+/*   Updated: 2024/01/22 02:31:36 by madwingg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		count_strings(char const *s, char c);
-int		calc_str_start(char const *str, char del, int last_end);
-int		calc_str_end(char const *str, char del, int str_start);
-char	*str_alloc(char const *src_str, char del, int nr_str);
 char	**ft_split(char const *s, char c);
+char	*ft_skip(char *s, char c, int i);
+int		ft_count_word(char *s, char c);
+char	**ft_free(char **result);
+char	*ft_wordcut(char *s, char c);
 
 char	**ft_split(char const *s, char c)
 {
-	int		n_str;
-	int		str_start;
-	int		str_end;
 	int		i;
 	char	**result;
+	char	*cpy;
 
-	if (s == NULL)
-		return (NULL);
-	n_str = count_strings(s, c);
-	result = malloc((n_str + 1) * sizeof(char *));
-	if (result == NULL)
-		return (NULL);
-	str_end = 0;
 	i = 0;
-	while (i < n_str)
+	if (!s)
+		return (0);
+	result = ft_calloc(ft_count_word((char *) s, c) + 1, sizeof(char *));
+	if (result == 0)
+		return (0);
+	if (ft_strlen(s) == 0)
+		return (result);
+	cpy = (char *)s;
+	cpy = ft_skip(cpy, c, 1);
+	while (*cpy != '\0')
 	{
-		str_start = calc_str_start(s, c, str_end);
-		str_end = calc_str_end(s, c, str_start);
-		*(result + i) = ft_substr(s, str_start, (str_end - str_start) + 1);
+		*(result + i) = ft_wordcut(cpy, c);
+		if (*(result + i) == 0)
+			return (ft_free(result));
+		cpy = ft_skip(cpy, c, 0);
+		cpy = ft_skip(cpy, c, 1);
 		i++;
-		str_end++;
 	}
-	*(result + i) = NULL;
 	return (result);
 }
 
-int	count_strings(char const *s, char c)
+char	*ft_skip(char *s, char c, int i)
+{
+	while (*s != '\0' && *s == c && i == 1)
+		s++;
+	while (*s != '\0' && *s != c && i == 0)
+		s++;
+	return (s);
+}
+
+int	ft_count_word(char *s, char c)
 {
 	int	i;
 
 	i = 0;
+	s = ft_skip(s, c, 1);
 	while (*s != '\0')
 	{
-		if (*s != c && *s != '\0')
-			i++;
-		while (*s != c && *s != '\0')
-			s++;
-		while (*s == c)
-			s++;
+		s = ft_skip(s, c, 0);
+		s = ft_skip(s, c, 1);
+		i++;
 	}
 	return (i);
 }
 
-int	calc_str_start(char const *str, char del, int last_end)
+char	**ft_free(char **str)
 {
-	int	str_start;
+	int	i;
 
-	str_start = last_end;
-	while (*(str + str_start) == del)
+	i = 0;
+	while (*(str + i))
 	{
-		str_start++;
+		free(*(str + i));
+		i++;
 	}
-	return (str_start);
+	free(str);
+	return (0);
 }
 
-int	calc_str_end(char const *str, char del, int str_start)
+char	*ft_wordcut(char *s, char c)
 {
-	int	str_end;
+	int		i;
+	char	*result;
 
-	str_end = str_start;
-	while (*(str + str_end + 1) != del && *(str + str_end + 1) != '\0')
+	i = 0;
+	while (*(s + i) != '\0' && *(s + i) != c)
+		i++;
+	result = ft_calloc(i + 1, sizeof(char));
+	if (result == 0)
+		return (0);
+	i = 0;
+	while (*(s + i) != '\0' && *(s + i) != c)
 	{
-		str_end++;
+		*(result + i) = *(s + i);
+		i++;
 	}
-	return (str_end);
+	*(result + i) = '\0';
+	return (result);
 }
